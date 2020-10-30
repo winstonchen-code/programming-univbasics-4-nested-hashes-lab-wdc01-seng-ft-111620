63919919487c198cdcfbf40f40a0ebfe70f3c155
Winston Chen <winstonchen.wm@gmail.com>
2020-10-30 00:59:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] 
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
