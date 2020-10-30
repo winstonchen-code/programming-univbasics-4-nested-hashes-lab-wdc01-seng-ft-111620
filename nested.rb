2eeff6d86878e7eddfa04623adf1677ce6cbfba3
Winston Chen <winstonchen.wm@gmail.com>
2020-10-30 01:05:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,6 +67,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		   :known_for => "Ruby",
+ 		   :languages => ["LISP", "C"]
+ 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto][:known_for]
+
 end
 
 def changing_alan
